ab2cb64a67dc131654d0a5bac015b36049d8192a
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-07-15 23:56:17 +0530

    changed like logic

--- app/api/hum/route.ts
+++ app/api/hum/route.ts
@@ -8,7 +8,9 @@ export async function POST(req: Request) {
 
 	const { hum } = await req.json();
 	try {
-		const data = await prisma.hum.create({ data: { content: hum, createdBy: currentUser.id } });
+		const data = await prisma.hum.create({
+			data: { content: hum, createdBy: currentUser.id, likes: [] },
+		});
 		return NextResponse.json(data);
 	} catch (e: any) {
 		return NextResponse.json({ message: "Error in humming" }, { status: 403 });
